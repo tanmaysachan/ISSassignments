61027dfc8c824f7b750753bf1d8a78279272e91c
tanmaysachan <tnmy.sachan@gmail.com>
2019-01-28 09:00:37 +0530

    fix script1.sh

--- script1.sh
+++ script1.sh
@@ -1,40 +1,26 @@
 #!/bin/bash
 
-maxLineLength=0
-
-for file in *; do
-	echo "$(date -r "$file" | cut -d " " -f 2-4) ./${file}"
+for file in `find *`; do
+	echo "$(date -r "$file" | cut -d " " -f 2-4) ${file}"
 done
 
 if [[ ! -e 2018111023.txt ]]; then
 	touch "2018111023.txt"
 fi
 
-coms="$(compgen -c "lo")"
-echo "$coms" > 2018111023.txt
+IFS=':' read -r -a arr <<< "$PATH"
 
-while read -r line || [[ -n "$line" ]]; do
-	char_count="$(echo "${line}" | wc -c)"
-	if [[ "$maxLineLength" < "$char_count" ]]; then
-		maxLineLength="$char_count"
-	fi
-done < 2018111023.txt
-# for file in /usr/bin/*; do
-# 	doced_file="$(echo $file | cut -d "/" -f 4)"
-# 	if [[ $doced_file == lo* ]]; then
-# 		char_count_file_name="$(echo "$doced_file" | wc -c)"
-# 		if [ "$maxLineLength" -lt "$char_count_file_name" ]; then
-# 			maxLineLength="$char_count_file_name"
-# 		fi
-# 		printf "${doced_file}\n" >> 2018111023.txt
-# 	fi
-# done
+for loc in "${arr[@]}"; do
+	for file in $loc/*; do
+		com=$(echo "$file" | cut -d"/" -f4)
+		if [[ "$com" =~	^lo ]]; then
+			printf "${com}: $(man -f ${com} | cut -d" " -f 13-)\n" >> 2018111023.txt
+		fi
+	done
+done
 
-echo "Max Line Length: ${maxLineLength}"
+echo "Max Line Length: $(wc -L 2018111023.txt | cut -d " " -f 1)"
 echo "Line count: $(wc -l 2018111023.txt | cut -d " " -f 1)"
-
-## under repair start
 
 sed -i -e "s/function/method/g" 2018111023.txt
 
-## under repair end
